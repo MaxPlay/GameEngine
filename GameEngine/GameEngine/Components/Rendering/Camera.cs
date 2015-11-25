72a60b7614408cdeb31e6ee1fd4a57a6e25ce819
MaxPlay <maxplayyt@gmail.com>
2015-11-25 18:16:06 +0100

    Added 2D-Camera
    Added Camerasupport
    Added Main-Camerasupport

--- GameEngine/GameEngine/Components/Rendering/Camera.cs
+++ GameEngine/GameEngine/Components/Rendering/Camera.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using GameEngine.Core;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,60 @@ using System.Threading.Tasks;
 
 namespace GameEngine.Components.Rendering
 {
-    class Camera
+    class Camera : Component
     {
+        public static Camera main;
+
+        private float zoom;
+        private Rectangle bounds;
+
+        public Matrix TransformMatrix
+        {
+            get
+            {
+                return 
+                    Matrix.CreateTranslation(new Vector3(-this.Transform.Position.X, -this.Transform.Position.Y, 0)) *
+                    Matrix.CreateRotationZ(this.Transform.Rotation) *
+                    Matrix.CreateScale(this.zoom) *
+                    Matrix.CreateTranslation(new Vector3(this.bounds.Width * 0.5f, this.bounds.Height * 0.5f, 0)
+                    );
+            }
+        }
+
+        public float Zoom { get { return this.zoom; } set { this.zoom = value; } }
+        public Viewport Viewport
+        {
+            get
+            {
+                return
+                    new Viewport(
+                        (int)(this.bounds.Width * -0.5f * (1f / this.zoom) + Transform.Position.X),
+                        (int)(this.bounds.Height * -0.5f * (1f / this.zoom) + Transform.Position.Y),
+                        (int)(this.bounds.Width * (1f / this.zoom)),
+                        (int)(this.bounds.Height * (1f / this.zoom))
+                        );
+            }
+        }
+
+        public Camera(GameObject gameObject)
+            : base(gameObject)
+        {
+            if (main == null)
+                main = this;
+
+            this.bounds = new Rectangle(0, 0, Bootstrap.graphics.PreferredBackBufferWidth, Bootstrap.graphics.PreferredBackBufferHeight);
+            this.zoom = 1;
+        }
+
+        public override void Reset()
+        {
+            this.bounds = new Rectangle(0, 0, Bootstrap.graphics.PreferredBackBufferWidth, Bootstrap.graphics.PreferredBackBufferHeight);
+            this.zoom = 1;
+        }
+
+        public Vector2 TranslatedCursorPosition()
+        {
+            return new Vector2(Mouse.GetState().X, Mouse.GetState().Y) + this.Transform.Position - new Vector2(this.bounds.Width * 0.5f, this.bounds.Height * 0.5f);
+        }
     }
 }
