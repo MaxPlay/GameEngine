2c8620201be2038d9d86a52359944446c8dc49b6
MaxPlay <maxplayyt@gmail.com>
2015-11-29 02:09:51 +0100

    Created Fontgenerator Project
    The Fontgenerator Project is a side- project for the creation of
    bitmap-fonts via commandline.
    The reason for that is the fact that XNA converts TrueType (somehow) to
    Bitmap internally. I will do the same, since SpriteFont is not working
    without the usage of the contentpipeline.
    
    Added Project "FontGenerator"
    Added FontTemplate for easier setup
    Added Converter to mange the conversion

---
+++ FontGenerator/FontTemplate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FontGenerator
+{
+    class FontTemplate
+    {
+        public string Fontname;
+        public bool Bold;
+        public bool Italic;
+        public bool Underline;
+        public int Size;
+
+        public FontTemplate(string name)
+        {
+            this.Fontname = name;
+            this.Bold = false;
+            this.Italic = false;
+            this.Underline = false;
+            this.Size = 10;
+        }
+
+        public Font CreateFont()
+        {
+            FontStyle style = Bold ? FontStyle.Bold : FontStyle.Regular;
+            style = Italic ? style | FontStyle.Italic : style;
+            style = Underline ? style | FontStyle.Underline : style;
+
+            return new Font(Fontname, Size, FontStyle.Bold, GraphicsUnit.Pixel);
+        }
+    }
+}
---
+++ FontGenerator/Programm.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FontGenerator
+{
+    class Programm
+    {
+        [DllImport("kernel32.dll")]
+        static extern int FreeConsole();
+
+        public static void Main(string[] args)
+        {
+            new Converter(args);
+        }
+    }
+}
