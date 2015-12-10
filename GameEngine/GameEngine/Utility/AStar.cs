5d6be333137207b50efb67566c93ba5455ac8699
MaxPlay <maxplayyt@gmail.com>
2015-12-10 01:44:11 +0100

    Pathfinding
    Added Pathfinding Project
    The reason is: I want to outsource this to make it easier to test. There
    is no much yet, but just to save this.

--- GameEngine/GameEngine/Utility/AStar.cs
+++ GameEngine/GameEngine/Utility/AStar.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace GameEngine.Utility
 {
     public static class AStar
     {
+        public static List<Point> Search(Point dimension, ref bool[] map, Point start, Point end)
+        {
+            List<Point> OpenList = new List<Point>();
+            List<Point> ClosedList = new List<Point>();
 
+            return ClosedList;
+        }
+
+        public static List<Point> Search(Point dimension, ref bool[,] map, Point start, Point end)
+        {
+            List<Point> OpenList = new List<Point>();
+            List<Point> ClosedList = new List<Point>();
+
+            return ClosedList;
+        }
+
+        public static bool CheckPath(Point dimension, ref bool[] map, Point start, Point end)
+        {
+            return (Search(dimension, ref map, start, end).Count != 0);
+        }
+
+        public static bool CheckPath(Point dimension, ref bool[,] map, Point start, Point end)
+        {
+            return (Search(dimension, ref map, start, end).Count != 0);
+        }
     }
 }
---
+++ Pathfinding/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Pathfinding
+{
+    internal class Node
+    {
+        private Point location;
+        private bool isWalkable;
+        private float g;
+        private float h;
+
+        public bool IsWalkable { get { return this.isWalkable; } set { this.isWalkable = value; } }
+        public float G { get { return this.g; } }
+        public float H { get { return this.h; } }
+        public float F { get { return this.h + this.g; } }
+        public Point Location { get { return this.location; } }
+    }
+}
