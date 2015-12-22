5d93c8114f6b1d2b9eebaacd049f2aa992b34199
MaxPlay <maxplayyt@gmail.com>
2015-12-22 17:46:35 +0100

    Fontgenerator Update
    To support Bitmapfonts I started working on an input-system.
    But I am not yet finished.
    
    -Added Tabcontrol to toggle between System-Fonts and Spritefonts
    -Added PreviewWindow
    
    -Todo: Documentation for the tool itself!

--- FontGenerator/MainWindow.xaml.cs
+++ FontGenerator/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace FontGenerator
 {
@@ -21,6 +25,8 @@ namespace FontGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Bitmap bitmapFont;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -65,7 +71,7 @@ namespace FontGenerator
 
         private void UpdatePreview()
         {
-            tbPreview.FontFamily = new FontFamily((string)combFonts.SelectedValue);
+            tbPreview.FontFamily = new System.Windows.Media.FontFamily((string)combFonts.SelectedValue);
             tbPreview.FontSize = combSize.SelectedIndex + 1;
             tbPreview.FontWeight = (cbBold.IsChecked ?? false) ? FontWeights.Bold : FontWeights.Normal;
             tbPreview.FontStyle = (cbItalic.IsChecked ?? false) ? FontStyles.Italic : FontStyles.Normal;
@@ -80,6 +86,78 @@ namespace FontGenerator
         private void combobox_ValuesChanged(object sender, EventArgs e)
         {
             UpdatePreview();
+        }
+
+        private void MainTabcontrol_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (MainTabcontrol.SelectedIndex)
+            {
+                case 0:
+                    this.Height = 231.2;
+                    this.Width = 547.6;
+                    break;
+                case 1:
+                    this.Height = 1000;
+                    this.Width = 547.6;
+                    break;
+            }
+        }
+
+        private void sldY_ud_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            tbY_ud.Text = sldY_ud.Value.ToString("F0");
+        }
+
+        private void sldX_ud_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            tbX_ud.Text = sldX_ud.Value.ToString("F0");
+        }
+
+        private void tb_ud_TextChanged(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"^(\d)+$");
+            e.Handled = !(regex.IsMatch(e.Text) && ((TextBox)sender).Text.Length < 4);
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            grd_UCD.IsEnabled = true;
+        }
+
+        private void RadioButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            grd_UCD.IsEnabled = false;
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "BMP Files (*.bmp)|*.bmp|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif|TIFF Files (*.tiff)|*.tiff";
+            bool? success = fd.ShowDialog(); 
+            if (success.HasValue && success.Value)
+            {
+                bitmapFont = (Bitmap)Bitmap.FromStream(fd.OpenFile());
+                tbResource.Text = fd.FileName;
+            }
+
+            btnPreview.IsEnabled = (bitmapFont != null);
+        }
+
+        private void btnReload_Click(object sender, RoutedEventArgs e)
+        {
+            if(File.Exists(tbResource.Text))
+            {
+                bitmapFont = (Bitmap)Bitmap.FromStream(File.OpenRead(tbResource.Text));
+            }
+
+            btnPreview.IsEnabled = (bitmapFont != null);
+        }
+
+        private void btnPreview_Click(object sender, RoutedEventArgs e)
+        {
+            PreviewWindow preview = new PreviewWindow();
+            preview.bitmapFont = this.bitmapFont;
+            preview.ShowDialog();
         }
     }
 }
---
+++ FontGenerator/PreviewWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FontGenerator
+{
+    /// <summary>
+    /// Interaction logic for PreviewWindow.xaml
+    /// </summary>
+    public partial class PreviewWindow : Window
+    {
+        public Bitmap bitmapFont;
+
+        public PreviewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+            bitmapFont.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            ms.Position = 0;
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.StreamSource = ms;
+            bi.EndInit();
+            this.ivPreview.Source = bi;
+            this.ivPreview.Width = bi.Width;
+            this.ivPreview.Height = bi.Height;
+        }
+    }
+}
